0220da35e837ef0c30951573ccdc0c7141c0731f
Gabriel <gabrielkralik@outlook.com>
2021-11-21 22:53:07 -0500

    update for moving

---
+++ routes/cool.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'You\'re so cool' });
+});
+
+module.exports = router;
