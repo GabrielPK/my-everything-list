fa4750897b5e7da4496e483f07724f879679f13f
root <gabrielkralik@outlook.com>
2021-12-19 01:27:09 +0000

    update entry_create_post and get

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -1,4 +1,5 @@
 var Entry = require('../models/entry');
+const { body,validationResult } = require("express-validator");
 
 exports.index = function(req, res) {
 	res.send('NOT IMPLEMENTED: Site Home Page');
@@ -15,15 +16,55 @@ exports.entry_detail = function(req, res) {
 };
 
 // Display entry create form on GET 
-exports.entry_create_get = function(req, res) {
-	res.send('NOT IMPLEMENTED: Entry create GET');
+exports.entry_create_get = function(req, res, next) {
+	res.render('entry_form', { title: 'Create Entry' });
 };
-
 
 // Handle entry create on POST.
-exports.entry_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Entry create POST');
-};
+exports.entry_create_post = [
+
+    // Validate and sanitize fields.
+    body('title').trim().isLength({ min: 1 }).escape().withMessage('Title must be specified.'),
+    body('link').trim().isLength({ min: 1 }).escape().withMessage('Link must be specified.'),
+    body('start_date', 'Invalid date of birth').isISO8601().toDate(),
+    body('finish_date', 'Invalid date of death').isISO8601().toDate(),
+    body('media_type').trim().isLength({ min: 1 }).escape().withMessage('Media Type must be specified.'),
+    body('is_fiction').trim().isBoolean().withMessage('Fiction status must be specified.'),
+    body('notes').trim().isLength({ min: 1 }).escape().withMessage('Notes must be specified.'),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('entry_form', { title: 'Create Entry', entry: req.body, errors: errors.array() });
+            return;
+        }
+        else {
+            // Data from form is valid.
+
+            // Create an Author object with escaped and trimmed data.
+            var entry = new Entry(
+                {
+                    title: req.body.title, 
+                    link: req.body.link, 
+                    start_date: req.body.start_date, 
+                    finish_date: req.body.finish_date, 
+                    media_type: req.body.media_type, 
+                    is_fiction: req.body.is_fiction, 
+                    notes: req.body.notes
+                });
+            author.save(function (err) {
+                if (err) { return next(err); }
+                // Successful - redirect to new author record.
+                res.redirect(entry.url);
+            });
+        }
+    }
+];
 
 // Display entry delete form on GET.
 exports.entry_delete_get = function(req, res) {
